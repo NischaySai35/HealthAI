ca5ed6475d842fcbb8709847940d7898153ddc56
Prachi <archeyyadav111@gmail.com>
2025-12-21 21:51:46 +0530

    Add files via upload

---
+++ healthai/src/test/java/com/healthai/HealthaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
